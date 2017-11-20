e0501da5fb8ab54cfb9c1c0e448bcb1906087497
汤圆 <chenxinyuan0842@126.com>
2017-11-20 09:52:09 +0800

    Add Beian Info

--- include/bottom.php
+++ include/bottom.php
@@ -5,6 +5,6 @@
 		<a style="text-decoration:underline;" onclick="alert('页面不适配你的移动设备？\n内容显示出现不恰当的换行？\n\n欢迎发送邮件到buaarsemmpp@126.com进行反馈～\n\n邮件请包括以下内容：\n· 学号/工作证号\n· 使用的移动设备型号\n· 使用的浏览器\n· 页面截图\n\n感谢反馈，我们将在维护时修正！');">	UnFit Devices?</a><br/>
 		Developed By 汤圆［13141009 / ChenTy95］<br/>
 		Now Established on TENCENT® Cloud Service<br/>
-		<a href="devlog.php">View Development Log</a>／<a href="http://www.miitbeian.gov.cn/">鲁ICP备????????号</a><br/>
+		<a href="devlog.php">View Development Log</a>／<a href="http://www.miitbeian.gov.cn/">鲁ICP备17050084号</a><br/>
 		</div>
 	</div>
